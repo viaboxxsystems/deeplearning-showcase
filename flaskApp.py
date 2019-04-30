9f72d0075a23c510ff49504bc156b258c660bc13
haitham-bjanthala <haitham.bjanthalah@viaboxx.de>
2019-04-30 13:47:20 +0200

    changes to classification flask app.
    adding trained models for ease of reproduction on other machines.

--- flaskApp.py
+++ flaskApp.py
@@ -10,6 +10,8 @@ from datetime import datetime
 from flask import Flask, request, jsonify, flash, redirect
 import tensorflow as tf
 
+from tensorflow.python import keras
+
 logger = logging.getLogger(__name__)
 logging.basicConfig()
 logger = logging.getLogger("flask_app")
@@ -22,7 +24,7 @@ net_models = dict()
 
 global graph, model
 graph = tf.compat.v1.get_default_graph()
-#tf.compat.v1.disable_eager_execution()
+tf.compat.v1.disable_eager_execution()
 
 
 def init_models():
@@ -33,10 +35,10 @@ def init_models():
 
     for model_name in models:
         if model_name.startswith("Mobile"):
-            with tf.keras.utils.CustomObjectScope({'relu6': tf.keras.layers.ReLU(6.), 'DepthwiseConv2D': tf.keras.layers.DepthwiseConv2D}):
-                net_models[model_name] = load_model(model_name)
+            with keras.utils.CustomObjectScope({'relu6': keras.layers.ReLU(6.), 'DepthwiseConv2D': keras.layers.DepthwiseConv2D}):
+                net_models[model_name] = load_model2(model_name)
         else:
-            net_models[model_name] = load_model(model_name)
+            net_models[model_name] = load_model2(model_name)
         logger.info('Loaded model and weights for ' + model_name)
     logger.info('all models loaded!')
 
@@ -47,9 +49,12 @@ def predict(cnn_name, image):
         x = tf.keras.preprocessing.image.img_to_array(resized_image)
         x = np.expand_dims(x, axis=0)
         post_processed_input_images = np.vstack([x])
-
+        #####
+        # with tf.keras.utils.CustomObjectScope({'relu6': tf.keras.layers.ReLU(6.), 'DepthwiseConv2D': tf.keras.layers.DepthwiseConv2D}):
+        #     net_models[cnn_name] = load_model(cnn_name)
+        #####
         # predict output
-        logger.warning(net_models)
+        logger.warning(net_models[cnn_name])
         logger.warning(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
         output_probability = net_models[cnn_name].predict(post_processed_input_images)
         output_classes = output_probability.argmax(axis=-1)
@@ -58,8 +63,13 @@ def predict(cnn_name, image):
 
 def load_model(model_name: str):
     with open('model_' + model_name + '_architecture.json', 'r') as f:
-        net_model = tf.keras.models.model_from_json(f.read())
+        net_model = keras.models.model_from_json(f.read())
     net_model.load_weights('model-' + model_name + '-final.h5')
+    return net_model
+
+
+def load_model2(model_name: str):
+    net_model = keras.models.load_model('model-' + model_name + '-final.h5old')
     return net_model
 
 
